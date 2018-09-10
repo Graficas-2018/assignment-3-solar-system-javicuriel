26f49d612b596b21b311c4a5d5fc4f5ea76e2352
javicuriel <javiercuriel94@gmail.com>
2018-09-10 00:34:07 -0500

    Finished

--- solarsystem.js
+++ solarsystem.js
@@ -2,41 +2,56 @@ var currentTime = Date.now()
 var duration = 10000;
 var images_path = 'images/'
 var system = null;
+var sun_scene_lights = [];
+var sun_intensity = 1;
+
 
 class SystemElement{
 
-  constructor(scale, texture_filename, normal_filename) {
+  constructor(scale, texture_filename, normal_filename, bump = false, map_scale = .2) {
     this.group = new THREE.Object3D;
-    this.texture = new THREE.TextureLoader().load(images_path+texture_filename);
-    this.normal = null;
-    if(normal_filename){
-      this.normal = new THREE.TextureLoader().load(images_path+normal_filename);
-    }
-    this.material = new THREE.MeshPhongMaterial({ map: this.texture, normalMap: this.normal});
-    this.element = new THREE.Mesh(this.constructor.geometry, this.material);
-    this.element.visible = true;
     this.scale = scale;
-    this.element.scale.set(scale, scale ,scale);
-    this.group.add(this.element);
-    this.constructor.all.push(this);
+    if(texture_filename){
+      this.texture = new THREE.TextureLoader().load(images_path+texture_filename);
+      this.normal = null;
+      if(normal_filename){
+        this.normal = new THREE.TextureLoader().load(images_path+normal_filename);
+      }
+      if(bump){
+        this.material = new THREE.MeshPhongMaterial({ map: this.texture, bumpMap: this.normal});
+        this.material.bumpScale = map_scale;
+      }
+      else{
+        this.material = new THREE.MeshPhongMaterial({ map: this.texture, normalMap: this.normal});
+        this.material.normalScale.set( map_scale,map_scale);
 
+      }
+
+
+      this.element = new THREE.Mesh(this.constructor.geometry, this.material);
+      this.element.visible = true;
+      this.element.scale.set(scale, scale ,scale);
+      this.group.add(this.element);
+
+
+    }
+    this.constructor.all.push(this);
     this.orbital_group = new THREE.Object3D;
     this.orbital_group.add(this.group)
+
+
   }
   add_orbital_element(element, radius, show_path = true) {
     if (show_path){
-      var path_geometry = new THREE.TorusGeometry(radius, .05, 8, 360 );
+      var path_geometry = new THREE.TorusGeometry(radius, .1, 8, 360 );
       var path = new THREE.Mesh( path_geometry, this.constructor.path_material );
       path.rotation.x += Math.PI /2
       this.group.add(path)
-
     }
 
     element.group.position.set(radius, 0,0);
     this.group.add(element.orbital_group);
-    
-    // element.element.position.set(radius, 0,0);
-    // this.group.add(element.group)
+
   }
   add_multiple_orbital_elements(elements){
     for (var i = 0; i < elements.length; i++) {
@@ -46,40 +61,8 @@ class SystemElement{
 }
 // Set geometry as class variable
 SystemElement.geometry = new THREE.SphereGeometry(2, 20, 20);
-SystemElement.path_material = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
+SystemElement.path_material = new THREE.MeshBasicMaterial( { color: 0xffffff } );
 SystemElement.all = [];
-
-class SolarSystem {
-  constructor(sun) {
-    this.sun = sun;
-    this.planets = [];
-    this.groups = [];
-  }
-  add_planet(planet, radius){
-    var group = new THREE.Object3D;
-    planet.group.position.set(radius, 0,0);
-    group.add(planet.group)
-
-
-    // var path_geometry = new THREE.TorusGeometry(radius, .05, 8, 360 );
-    // var path = new THREE.Mesh( path_geometry, this.constructor.path_material );
-    // // 90 degree rotation
-    // path.rotation.x += Math.PI /2
-    // group.add(path)
-    this.groups.push(group);
-
-  }
-  add_planets(planets){
-    for (var i = 0; i < planets.length; i++) {
-      // console.log(this.scale);
-      // console.log((planets[i].scale*3) * (i+1) +planets[i].scale);
-      this.add_planet(planets[i], (i+1)*35);
-
-    }
-    console.log(this.groups);
-  }
-}
-SolarSystem.path_material = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
 
 
 function animate() {
@@ -88,29 +71,109 @@ function animate() {
     currentTime = now;
     var fract = deltat / duration;
     var angle = Math.PI * 2 * fract;
-    var movement = now * 0.001;
-
-    // for (var i = 0; i < system.groups.length; i++) {
-    //   system.groups[i].rotation.y += angle/i;
-    // }
 
     for (var i = 0; i < SystemElement.all.length; i++) {
       // Rotation over orbiting element
       // Farther away the slower they go
-      SystemElement.all[i].group.rotation.y += angle/(i+1);
+      SystemElement.all[i].group.rotation.y += angle/SystemElement.all[i].scale;
+      // SystemElement.all[i].group.rotation.y += angle*SystemElement.all[i].scale/2;
+
       // Rotation over their own axis
       // The bigger they are the slower they rotate
-      // SystemElement.all[i].element.rotation.y += angle/SystemElement.all[i].scale;
-
       SystemElement.all[i].orbital_group.rotation.y += angle/(i+1);
+
+    }
+
+
+    for (var i = 0; i < sun_scene_lights.length; i++) {
+      sun_scene_lights[i].intensity += sun_intensity*100/duration
+    }
+
+    // Last element
+    if(sun_scene_lights[3].intensity > 8 || sun_scene_lights[3].intensity < 5){
+      sun_intensity = sun_intensity*-1;
 
     }
 
 }
 
+// For the sun to have its own colors
+function createSunScene() {
+  sun_scene = new THREE.Scene();
+  sun = new SystemElement(20, 'lava2.jpg', 'lavaNormal.png', false ,1);
+
+  // Add a directional light to show off the object
+  var top_light = new THREE.DirectionalLight( 0xffffff, 8);
+  var bottom_light = new THREE.DirectionalLight( 0xffffff, 8);
+  var front_light = new THREE.DirectionalLight( 0xffffff, 8);
+  var back_light = new THREE.DirectionalLight( 0xffffff, 8);
+
+  front_light.position.set(100, 0, 0);
+  back_light.position.set(-100, 0, 0);
+  top_light.position.set(0, 100, 0);
+  bottom_light.position.set(0, -100, 0);
+
+  bottom_light.target = sun.element;
+  top_light.target = sun.element;
+  front_light.target = sun.element;
+  back_light.target = sun.element;
+
+  sun_scene.add(bottom_light);
+  sun_scene.add(top_light);
+  sun_scene.add(front_light);
+  sun_scene.add(back_light);
+
+  sun_scene_lights = [top_light,bottom_light,front_light,back_light];
+
+  sun_scene.add(sun.group);
+
+  return sun_scene;
+}
+
+function add_ambient_light(intensity = 1) {
+  // Ambient lighting
+  // Red
+  var light = new THREE.AmbientLight( 0xff0000);
+  light.intensity = .01*intensity
+  scene.add( light );
+  // Orange
+  var light = new THREE.AmbientLight( 0xffb200);
+  light.intensity = .1*intensity
+  scene.add( light );
+  // White-Yellow
+  var light = new THREE.AmbientLight( 0xfeffe2);
+  light.intensity = .4*intensity
+  scene.add( light );
+}
+
+function get_star_field() {
+  // Star starField taken from documentation
+  var starsGeometry = new THREE.Geometry();
+  for ( var i = 0; i < 40000; i ++ ) {
+     var star = new THREE.Vector3();
+     // Taken from https://gist.github.com/KhanMaytok/d76f58c55704b7ce6140
+     var theta = THREE.Math.randFloatSpread(360);
+     var phi = THREE.Math.randFloatSpread(360);
+     star.x = 500 * Math.sin(theta) * Math.cos(phi);
+     star.y = 500 * Math.sin(theta) * Math.sin(phi);
+     star.z = 500 * Math.cos(theta);
+     starsGeometry.vertices.push( star );
+
+
+  }
+  var starsMaterial = new THREE.MeshPhongMaterial({ color: 0xfeffe2,emissive: 0xffffff, emissiveIntensity:.6});
+  // var starsMaterial = new THREE.PointsMaterial( { color: 0x888888, size:10 } );
+  var starField = new THREE.Points( starsGeometry, starsMaterial );
+  return starField;
+
+}
+
 function run() {
     requestAnimationFrame(function() { run(); });
+    renderer.clear();
+    // renderer.render( backgroundScene, backgroundCamera );
     renderer.render( scene, camera );
+    renderer.render( sun_scene, camera );
     // Spin the cube for next frame
     animate();
 }
@@ -120,53 +183,115 @@ function createScene(canvas) {
   renderer = new THREE.WebGLRenderer( { canvas: canvas, antialias: true } );
   // Set the viewport size
   renderer.setSize(canvas.width, canvas.height);
+  renderer.autoClear = false;
+
   // Create a new Three.js scene
   scene = new THREE.Scene();
   // Set the background color
   scene.background = new THREE.Color( 0,0,0);
+  // scene.background =new THREE.TextureLoader().load('images/background3.png' );
   // Add  a camera so we can view the scene
   camera = new THREE.PerspectiveCamera( 45, canvas.width / canvas.height, 1, 4000 );
-  camera.position.z = 500;
+  camera.position.z = 100;
   // camera.position.x = 200;
+
+
   scene.add(camera);
+  // camera.layers.enable( 1 );
+  camera.layers.enable( 2 );
 
-  // Create a group to hold all the objects
-  solar_system = new THREE.Object3D;
-  light = new THREE.AmbientLight ( 0xffffff );
-  solar_system.add(light);
-
-
-  // emissive sun
-  // orbit controls
-  sun = new SystemElement(15, 'sunmap.jpg', null);
-  mercury = new SystemElement(0.9, 'mercurymap.jpg', null);
-  venus = new SystemElement(1.7, 'venusmap.jpg', null);
-  earth = new SystemElement(1.8, 'earth_atmos_2048.jpg', 'earth_normal_2048.jpg');
-  moon = new SystemElement(.3, 'moon_texture.jpg', null);
-  mars = new SystemElement(1.3, 'mars_1k_color.jpg', null);
-  jupiter = new SystemElement(6, 'jupitermap.jpg', null);
-  saturno = new SystemElement(5, 'saturnmap.jpg', null);
-  urano = new SystemElement(4.7, 'uranusmap.jpg', null);
-  neptune = new SystemElement(4.5, 'neptunemap.jpg', null);
-  pluto = new SystemElement(0.8, 'plutomap1k.jpg', null);
+  var controls = new THREE.OrbitControls( camera );
+  controls.update();
 
 
 
-  // earth.add_orbital_element(moon, 0, false);
-  sun.add_multiple_orbital_elements([mercury,venus,earth,mars,jupiter,saturno,urano,neptune,pluto])
-  // system = new SolarSystem(sun);
-  // system.add_planets([mercury,venus,earth,mars,jupiter,saturno,urano,neptune,pluto]);
+  var sun_scene = createSunScene();
+
+  // Set Sun Light
+  var sunlight = new THREE.PointLight( 0xfeffe2, 2, 100000 );
+  sunlight.distance= 600
+  scene.add( sunlight );
+
+  add_ambient_light(.2)
+
+  scale_all = 1.5
+  sun_placer = new SystemElement(17);
+  mercury = new SystemElement(scale_all*0.9, 'mercurymap.jpg', 'mercurybump.jpg',true);
+  venus = new SystemElement(scale_all*1.7, 'venusmap.jpg', 'venusbump.jpg',true);
+  earth = new SystemElement(scale_all*1.8, 'earth_atmos_2048.jpg', 'earth_normal_2048.jpg');
+  moon = new SystemElement(scale_all*.3, 'moon_texture.jpg', 'moon_bump.jpg',true);
+  mars = new SystemElement(scale_all*1.3, 'mars_1k_color.jpg', 'mars_1k_normal.jpg');
+  jupiter = new SystemElement(scale_all*6, 'jupitermap.jpg', null);
+  saturno = new SystemElement(scale_all*5, 'saturnmap.jpg', null);
+  urano = new SystemElement(scale_all*4.7, 'uranusmap.jpg', null);
+  neptune = new SystemElement(scale_all*4.5, 'neptunemap.jpg', null);
+  pluto = new SystemElement(scale_all*0.8, 'plutomap1k.jpg', 'plutobump1k.jpg');
+
+
+  sun_placer.add_multiple_orbital_elements([mercury,venus,earth,mars,jupiter,saturno,urano,neptune,pluto])
 
   earth.add_multiple_orbital_elements([moon])
 
-  // scene.add(earth.group);
+  var starField = get_star_field();
 
-  // system.groups.forEach(function(group){
-  //   scene.add(group);
-  // })
+  scene.add(starField);
+  scene.add(sun_placer.group);
+  // scene.add(earth.group)
 
-  scene.add(sun.group);
-  scene.add(solar_system);
+  // var customMaterial = new THREE.ShaderMaterial(
+  // {
+  //   uniforms:
+  //   {
+  //     "c":   { type: "f", value: 0.3 },
+  //     "p":   { type: "f", value: 3.2 },
+  //     glowColor: { type: "c", value: new THREE.Color(0xffffff) },
+  //     viewVector: { type: "v3", value: camera.position }
+  //   },
+  //   // vertexShader:   document.getElementById( 'vertexShader'   ).textContent,
+  //   // fragmentShader: document.getElementById( 'fragmentShader' ).textContent,
+  //   side: THREE.BackSide,
+  //   blending: THREE.AdditiveBlending,
+  //   transparent: true
+  // });
+  //
+  // var moonGlow = new THREE.Mesh( SystemElement.geometry.clone(), customMaterial.clone() );
+  // moonGlow.position = earth.element.position;
+	// moonGlow.scale.multiplyScalar(1.2);
+	// scene.add( moonGlow );
+
+
+  // instantiate a loader
+var loader = new THREE.OBJLoader();
+
+// load a resource
+loader.load('images/asteroid.obj',function (object) {
+    asteroids = new THREE.Object3D;
+    texture = new THREE.TextureLoader().load("images/astroid_texture.jpg");
+    // material = new THREE.MeshPhongMaterial({ map: texture});
+    object.children[0].material = new THREE.MeshPhongMaterial({ map: texture});
+    for ( var i = 0; i < 1500; i ++ ) {
+       var asteroid = object.clone()
+       var theta = THREE.Math.randFloatSpread(360);
+       var phi = THREE.Math.randFloatSpread(360);
+
+       var r = 225
+       sizex = Math.abs(THREE.Math.randFloatSpread(.1))
+
+       asteroid.scale.set(sizex,sizex,sizex);
+       asteroid.position.x = (r + 10*Math.cos(theta))*Math.cos(phi)
+       asteroid.position.y = (r + 10*Math.cos(theta*THREE.Math.randFloatSpread(1000)))*Math.sin(phi)
+       asteroid.position.z = 10*Math.sin(theta*20/i);
+       asteroids.add( asteroid );
+
+
+    }
+    asteroids.rotation.x += Math.PI /2
+    sun_placer.group.add(asteroids)
+
+
+	});
+
+
 
 
 
